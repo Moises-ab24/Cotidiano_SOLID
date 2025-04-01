c25207c08d15d5f33e4c47cf0f4bf713dd4eb9a2
Moises-ab24 <moi24ad@gmail.com>
2025-04-01 10:12:13 -0600

    isp Cotidiano

---
+++ src/isp/Ejercicio_2_sin.kt
@@ -0,0 +1,23 @@
+package isp_sin
+
+interface Empleado {
+    fun trabajar()
+    fun administrar()
+    fun auditar()
+}
+class Desarrollador : Empleado {
+    override fun trabajar() {
+        println("Desarrollando software...")
+    }
+    override fun administrar() {
+        throw UnsupportedOperationException("No puede administrar")
+    }
+    override fun auditar() {
+        throw UnsupportedOperationException("No puede auditar")
+    }
+}
+fun main() {
+    val dev = Desarrollador()
+    dev.trabajar()
+}
+
