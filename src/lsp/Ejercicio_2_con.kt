ca0c6af0d8f47a15e5e08d0be8898102a31db29a
Moises-ab24 <moi24ad@gmail.com>
2025-04-01 09:26:16 -0600

    LSP Cotidiano

---
+++ src/lsp/Ejercicio_2_con.kt
@@ -0,0 +1,16 @@
+package lsp_con
+
+open class Dispositivo
+open class Encendidos : Dispositivo() {
+    open fun encender() {
+        println("Encendiendo dispositivo encendible...")
+    }
+}
+
+class Televisor : Encendidos()
+class Radio : Dispositivo()
+
+fun main() {
+    val dispositivo = Televisor()
+    dispositivo.encender()
+}
